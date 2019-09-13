6a2344747420a46f2b070d58a8d539a83ef65754
Xinran Wang <wxr001@gmail.com>
2019-09-12 20:35:08 -0500

    remove js

--- src/algebra/inverse.js
+++
@@ -1,8 +0,0 @@
-import { mat } from '../matrix/matrix';
-import * as mathjs from 'mathjs';
-function inverse(inmat) {
-    if (inmat.rows != inmat.cols)
-        throw new Error("A must be a suqare matrix");
-    return new mat(mathjs.inv(inmat.val));
-}
-export { inverse };
--- src/ml/mlUnitTest.js
+++
@@ -1,3 +0,0 @@
-import 'assert';
-export function machineLearningTest() {
-}
